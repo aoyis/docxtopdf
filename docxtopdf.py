7b9ad3a8c9788b2028cd7faa8c2b9e930b69afe3
aoyi <aoyi2000@gmail.com>
2026-09-21 23:10:10 +0800

    fix: fix infinite loop

--- docxtopdf.py
+++ docxtopdf.py
@@ -1,37 +1,33 @@
 import sys
 import os
+
 import comtypes.client
-print(os.getcwd())
+from loguru import logger
+
+
+logger.info(os.getcwd())
 wdFormatPDF = 17
-print(os.listdir)
+# print(os.listdir)
 
 
 def convert_in_dir(word):
-    dirspath = [x[0] for x in os.walk(os.getcwd())]
-    for ue in dirspath:
-        os.chdir(ue)
-        
-        for root, dirs, files in os.walk(os.getcwd()):
-            
-            for file in files:
-                if file.endswith(".docx"):
-                    
-                    file_to_convert = os.path.join(root, file)
-                    out_file = file_to_convert + ".pdf"
-                    
-                    doc = word.Documents.Open(file_to_convert)
-                    doc.SaveAs(out_file, FileFormat=wdFormatPDF)
-                   
-                    doc.Close()
-                     
-                    print("successfully convert " +  file_to_convert + " to pdf")
-            
-        dirspath = [x[0] for x in os.walk(os.getcwd())]
-        if len(dirspath) > 0:
-            convert_in_dir(word)
+    for root, _, files in os.walk(os.getcwd()):
+        os.chdir(root)
+        for file in files:
+            if file.endswith(".docx"):
+                source = os.path.join(root, file)
+                target = os.path.join(root, "".join(file.split(".")[:-1]) + ".pdf")
 
-    print("successfully convert all docx files to pdf")
+                # actual conversion
+                doc = word.Documents.Open(source)
+                doc.SaveAs(target, FileFormat=wdFormatPDF)
+                doc.Close()
+                    
+                logger.success(f"Successfully converted {source} to {target}!")
+
+    logger.success("Successfully converted all docx files to pdf!")
             
+# TODO: might need to refactor and add single target file
             
 word = comtypes.client.CreateObject('Word.Application')
 convert_in_dir(word)
